2001192b2e9967791dfb35136555a209060e6709
Saia Fonua <saia.fonua@C02F20S7MD6M.local>
2021-08-04 22:55:36 -0600

    first commit

---
+++ src/hooks/usePhotoGallery.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { isPlatform } from "@ionic/react";
+
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from "@capacitor/camera";
+import { Filesystem, Directory } from "@capacitor/filesystem";
+import { Storage } from "@capacitor/storage";
+import { Capacitor } from "@capacitor/core";
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
+export function usePhotoGallery() {
+  const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
+  
+  const takePhoto = async () => {
+    const cameraPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+    const fileName = new Date().getTime() + ".jpeg";
+    const newPhotos = [
+      {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath,
+      },
+      ...photos,
+    ];
+    setPhotos(newPhotos);
+  };
+
+  return {
+    photos,
+    takePhoto,
+  };
+}
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,56 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonLabel, IonList, IonItem, IonListHeader, IonPage, IonRadio, IonRadioGroup, IonTitle, IonToolbar, IonText } from '@ionic/react';
+// import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const daysOfWeek = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+  const monthsOfYear = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const today = new Date();
+  const day = daysOfWeek[today.getDay()]
+  const month = monthsOfYear[today.getMonth()]
+  const date = today.getDate()
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Daily Goals</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Daily Goals</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonList>
+          <IonRadioGroup>
+            <IonListHeader>
+              <IonLabel>Social Media</IonLabel>
+            </IonListHeader>
+            <IonItem>
+              <IonLabel>
+                <IonText>
+                  <p>9pm - 9am</p>
+                </IonText>
+                <IonText color="medium">
+                  <h5>{`${day} ${month} ${date}`}</h5>
+                </IonText>
+              </IonLabel>
+              <IonRadio slot="start" value="time" color="success" />
+            </IonItem>
+            <IonItem>
+              <IonLabel>
+                <IonText>
+                  <p>Specific purpose</p>
+                </IonText>
+                <IonText color="medium">
+                  <h5>{`${day} ${month} ${date}`}</h5>
+                </IonText>
+              </IonLabel>
+              <IonRadio slot="start" value="time" color="success" />
+            </IonItem>
+          </IonRadioGroup>
+        </IonList>
       </IonContent>
     </IonPage>
   );
